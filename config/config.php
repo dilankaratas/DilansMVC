ee782d32e31087ec93be188ef5f388b250c8860f
dilankaratas <karatassdilan@gmail.com>
2022-04-25 04:28:41 +0300

    Changes

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+define('SITE_NAME', 'mvc-project');
+
+
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+define('DB_HOST', 'smtp.gmail.com');
+define('DB_USER', 'admin');
+define('DB_PASS', '123');
+define('DB_NAME', 'mvc');
